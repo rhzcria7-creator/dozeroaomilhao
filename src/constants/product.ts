720a96b31df2e8769fd4f9a0379ebf6b4450da35
rhzcria7-creator <2.24267762e+08+rhzcria7-creator@users.noreply.github.com>
2026-06-10 13:43:11 +0000

    Use clean QR Code image without Mercado Pago info

--- src/constants/product.ts
+++ src/constants/product.ts
@@ -29,7 +29,7 @@ export const PIX = {
   receiverName: "Rhian Augusto Reis Lopes",
   cpfMasked: "***.873.036-**",
   institution: "Mercado Pago",
-  qrCodeImage: "/qr-code-pix.png",
+  qrCodeImage: "/qr-code-pix-clean.png",
   // Código copia-e-cola PIX (BR Code EMV)
   copyPaste:
     "00020126580014br.gov.bcb.pix0136b1734aa3-6240-4d95-9194-33387d596f4a5204000053039865406129.905802BR5924Rhian Augusto Reis Lopes6009Sao Paulo62240520daqr3180513534991866630477BB",
